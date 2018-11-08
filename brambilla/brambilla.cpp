b2eb22aacfa1f517a0f44c7d55e4c650df78ee33
robmelfi <melfiroberto@gmail.com>
2018-11-08 16:16:44 +0100

    lettura input

--- brambilla/brambilla.cpp
+++ brambilla/brambilla.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
+int N;
+int abitanti[100];
+int costi[100];
+
 int main() {
-  cout << "Hello" << endl;
+  ifstream in("input.txt");
+  ofstream out("output.txt");
+  in >> N;
+  for (int i = 0; i < N; i++)
+    in >> costi[i];
+  for (int i = 0; i < N; i++)
+    in >> abitanti[i];
 }
